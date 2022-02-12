9f7ee2cf6e0995583135d75e57a0baceae589b0f
AllaBerkana <allaberkana@gmail.com>
2022-02-12 18:42:27 +0200

    commit_02 lesson_02

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+{
+    const item = 'апельсин';
+    const count = 20;
+    const category = 'фрукты';
+    const price = 120;
+
+    console.log('item: ', item);
+    console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+    const item = 'яблоко';
+    const count = 50;
+    const category = 'фрукты';
+    const price = 70;
+
+    console.log('item: ', item);
+    console.log(`общая сумма товара: ${count * price}`);
+}
+
